26da9db4c2494ad4c9433a7d249394bb72378022
Roisfaozi <roisfaozi55@gmail.com>
2024-03-13 01:04:26 +0700

    Fix: router configuration

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,9 @@ import AuthPages from '../ui/AuthPages'
 
 function RegisterPage() {
   return (
-    <AuthPages authAction='register' />
+    <>
+      <AuthPages authAction='register' />
+    </>
   )
 }
 
